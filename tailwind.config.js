8a91a880e4ea609b976ed8c191467b70c0e043a2
Enrique Marín <enriquemarin_sierra@hotmail.com>
2024-12-26 22:03:03 +0100

    Add custom font to priv/static folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 import daisyui from "daisyui";
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{gleam,mjs}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Kanit', ...fontFamily.sans]
+      }
+    },
   },
   plugins: [daisyui],
   daisyui: {
